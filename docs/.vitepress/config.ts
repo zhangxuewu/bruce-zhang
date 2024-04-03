5d718dbd9a5f0d5fccb6e80fcffb8dc3ca61703e
zhangxuewu <zhangxuewu@zhangxuewudeMac-mini.local>
2024-04-03 23:11:09 +0800

    修改个人信息

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/bruce-zhang-01' }
     ]
   }
 });
--- vitepress-starter/deploy.sh
+++ vitepress-starter/deploy.sh
@@ -15,6 +15,6 @@ git commit -m 'deploy'
 
 # Jacqueline712/mondaylab-blog 替换为自己的用户名和对应的仓库名
 # 意思为将master构建后的代码合并到gh-pages分支上，然后在gh-pages分支上部署~
-git push --set-upstream git@github.com:zhangxuewu/bruce-zhang.git main:gh-pages
+git push -f git@github.com:zhangxuewu/bruce-zhang.git main:gh-pages
 
 cd -
